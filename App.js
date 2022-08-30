b61022eff91edd1f222f50de8042a25496ef71ba
kindthr <kindtaher@gmail.com>
2022-08-30 13:31:34 +0100

    Second xommit

---
+++ App.js
@@ -0,0 +1,138 @@
+import React, { useState, useRef, useEffect } from 'react'
+//...
+import * as Notifications from 'expo-notifications'
+
+import { registerForPushNotificationsAsync,
+         schedulePushNotification,
+         scheduleSolutionPushNotification
+} from './src/services/push_notification'
+
+const Drawer = createDrawerNavigator()
+
+
+export default function App() {
+
+  const called = useRef(false)
+  const calledSoltionNotification = useRef(false)
+
+  const [currentScreen, setCurrentScreen] = useState("SplashScreen")
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+
+  
+
+ /**--------------------------End push notification test ----------------- */
+
+  useEffect(()=> {
+
+
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+      console.log(notification);
+     
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+
+      setNotification(response.notification)
+      console.log(response);
+      
+
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+
+    };
+
+  },[])
+
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      setCurrentScreen("StartScreen");
+    }, 5000)
+   
+  
+
+   // ...
+
+   handlerSystemNotification()
+   handlerSolutionotification()
+
+  }, []);
+
+
+  /** --------------------------- Start getting plants data  ------------------------------**/
+
+
+
+
+/** --------------------------- End getting plants data  ------------------------------**/
+
+
+
+ 
+    function handlerSystemNotification() {
+
+
+    if (!calledSoltionNotification.current && systemState == 'Auto' &&  solutionPump == 0) {
+
+      calledSoltionNotification.current = true
+      schedulePushNotification()
+      sendSMS()
+     
+      return () => {
+      
+  
+      };
+
+    }
+    if(calledSoltionNotification.current == "true" && solutionPump != 0) {
+
+      calledSoltionNotification.current = false
+
+   
+     }
+   
+
+  }
+
+  function handlerSolutionotification() {
+ 
+
+    if (!called.current && waterLevel <= 50) {
+
+      scheduleSolutionPushNotification()
+      called.current = true
+
+      return () => {
+        
+  
+      };
+      
+    } if(called.current == "true" && waterLevel >= 50) {
+     
+      called.current = false
+    }
+  }  
+ 
+ 
+  return (
+  
+      
+         <>
+         
+         </>
+
+  )
+
+}
---
+++ services/push_notification.js
@@ -0,0 +1,75 @@
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+      
+    }),
+  });
+
+  
+export async function registerForPushNotificationsAsync() {
+
+    let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
+
+  export function scheduleSolutionPushNotification() {
+    Notifications.scheduleNotificationAsync({
+     content: {
+
+       title: "🔔 Alerte",
+       body: 'Le niveau d\'eau est bas, pouvez-vous vérifier votre système',
+       data: { data: 'goes here' },
+       sound: 'default',
+       vibrate: false,
+       priority: 'high',
+     },
+     trigger: { seconds: 3 },
+
+   });
+ }
+ export function schedulePushNotification() {
+  Notifications.scheduleNotificationAsync({
+     content: {
+
+       title: "⚠️ Avertissement",
+       body: 'Warning!! Vérifier l\'état de la pompe solution',
+       data: { data: 'goes here' },
+       sound: 'default',
+       vibrate: false,
+       priority: 'high',
+     },
+     trigger: { seconds: 3 },
+
+   });
+ }
